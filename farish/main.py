a54de4702b2ca538d8b0ba098471072535d7757e
fpunjani <fpunjani@ualberta.ca>
2020-11-22 20:59:09 -0500

    Temp display

---
+++ farish/main.py
@@ -0,0 +1,33 @@
+import pygame
+import sys
+
+WIDTH, HEIGHT = 800,800
+FPS=60
+
+def get_events():
+    global running
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running = False
+
+def update():
+    pass
+
+def draw():
+    window.fill(BACKGROUND)
+
+pygame.init()
+window= pygame.display.set_mode(WIDTH, HEIGHT)
+clock = pygame.time.Clock()
+
+running=True
+
+while running:
+    get_events()
+    update()
+    draw()
+    pygame.display.update()
+    clock.tick(FPS)
+
+pygame.quit()
+sys.exit()

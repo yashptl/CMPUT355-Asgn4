9cd5695dfb2690b4b86ba0711b4b2f48eaf49555
fpunjani <fpunjani@ualberta.ca>
2020-11-22 23:24:03 -0500

    update- window display

--- farish/main.py
+++ farish/main.py
@@ -1,7 +1,10 @@
 import pygame
 import sys
+from game_window_class import *
 
-WIDTH, HEIGHT = 800,800
+
+WIDTH, HEIGHT = 500,500
+BACKGROUND= (199,199,199)
 FPS=60
 
 def get_events():
@@ -11,14 +14,17 @@ def get_events():
             running = False
 
 def update():
-    pass
+    game_window.update()
+
 
 def draw():
     window.fill(BACKGROUND)
+    game_window.draw()
 
 pygame.init()
-window= pygame.display.set_mode(WIDTH, HEIGHT)
+window= pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
+game_window= game_window(window, 50,50)
 
 running=True
 
